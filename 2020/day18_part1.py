d18d82d34a7d48a37a5a6953e6563c986d6c8446
Buster2002 <mysteriousduck2002@gmail.com>
2020-12-05 19:50:48 +0100

    initial commit

---
+++ 2020/day18_part1.py
@@ -0,0 +1,12 @@
+
+"""
+Author: Buster
+Link: https://adventofcode.com/2020/day/18#part1
+"""
+INPUT = open('day18_input.txt', 'r', encoding='utf-8').read()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ 2020/day23_part2.py
@@ -0,0 +1,12 @@
+
+"""
+Author: Buster
+Link: https://adventofcode.com/2020/day/23#part2
+"""
+INPUT = open('day23_input.txt', 'r', encoding='utf-8').read()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
