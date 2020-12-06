99c44334ebc44a116ef9239c8403461134b2800b
Buster2002 <mysteriousduck2002@gmail.com>
2020-12-06 15:25:34 +0100

    day 6

--- 2020/day6_part1.py
+++ 2020/day6_part1.py
@@ -3,9 +3,26 @@ Author: Buster
 Link: https://adventofcode.com/2020/day/6#part1
 """
 INPUT = open('day6_input.txt', 'r', encoding='utf-8').read()
+GROUPS = list(map(lambda x: x.replace('\n', ''), INPUT.strip().split('\n\n')))
 
-def main():
-    pass
+# oneliner: print(sum(len(set(g))for g in map(lambda x:x.replace('\n', ''),open('day6_input.txt').read().split('\n\n'))))
+
+class Evaluate():
+
+    @classmethod
+    def get_unique(cls, group: str) -> int:
+        '''Returns the amount of unique items in a string'''
+        return len(set(group))
+
+    def execute(self) -> int:
+        '''Returns the sum of the amount of unique yes answers for each group'''
+        total = 0
+
+        for group in GROUPS:
+            total += self.get_unique(group)
+
+        print(f'Answer day 6, part 1: {total}')
 
 if __name__ == '__main__':
-    main()
+    evaluator = Evaluate()
+    evaluator.execute()
--- 2020/day6_part2.py
+++ 2020/day6_part2.py
@@ -3,9 +3,26 @@ Author: Buster
 Link: https://adventofcode.com/2020/day/6#part2
 """
 INPUT = open('day6_input.txt', 'r', encoding='utf-8').read()
+GROUPS = list(map(lambda x: x.split('\n'), INPUT.strip().split('\n\n')))
 
-def main():
-    pass
+# oneliner: print(sum(len(set.intersection(*[set(m)for m in g]))for g in map(lambda x:x.split('\n'),open('day6_input.txt').read().split('\n\n'))))
+
+class Evaluate():
+
+    @classmethod
+    def get_unique(cls, group: list) -> int:
+        '''Returns the amount of unique items in a string'''
+        return len(set.intersection(*[set(member) for member in group]))
+
+    def execute(self) -> int:
+        '''Returns the sum of the amount of unique yes answers for each group'''
+        total = 0
+
+        for group in GROUPS:
+            total += self.get_unique(group)
+
+        print(f'Answer day 6, part 2: {total}')
 
 if __name__ == '__main__':
-    main()
+    evaluator = Evaluate()
+    evaluator.execute()
