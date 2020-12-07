c87407e4b6e215b07a7e1a68b09f5d376be9ea6c
Buster2002 <mysteriousduck2002@gmail.com>
2020-12-07 20:04:00 +0100

    doesn't even work lol

--- 2020/day7_part1.py
+++ 2020/day7_part1.py
@@ -3,9 +3,35 @@ Author: Buster
 Link: https://adventofcode.com/2020/day/7#part1
 """
 INPUT = open('day7_input.txt', 'r', encoding='utf-8').read()
+# this might be the stupidest way to do this but ok
+BAGS = {bag[0]: bag[1:] for bag in list(list(map(str.strip, l)) for l in map(lambda x: x.split(','), INPUT.replace('bags contain', ',').replace('bags', '').replace('bag', '').replace('.', '').translate(str.maketrans('', '', '0123456789')).splitlines()))}
 
-def main():
-    pass
+class Evaluate():
 
+    @classmethod
+    def get_amount(cls, bag: str) -> int:
+        '''Returns the amount of shiny gold bags a bag can eventually hold'''
+        golden = 0
+
+        def recurse(_bag):
+            for sub_bag in _bag:
+                if sub_bag == 'shiny gold':
+                    golden += 1
+                    break
+
+                recurse(BAGS.get(sub_bag))
+
+        return golden
+
+    def execute(self) -> int:
+        '''Prints the sum of bag colours that can eventually hold at least 1 shiny gold bag'''
+        total = 0
+
+        for bag in BAGS:
+            total += self.get_amount(bag)
+
+        print(f'Answer day 7, part 1: {total}')
+    
 if __name__ == '__main__':
-    main()
+    evaluator = Evaluate()
+    evaluator.execute()
